e0d9cb2141ae480781f8529fd3ceb3934ef201bf
Brian Cardarella <bcardarella@gmail.com>
2025-09-16 09:48:25 -0400

    Add vml request support

--- rails_test/app/controllers/counter_controller.rb
+++ rails_test/app/controllers/counter_controller.rb
@@ -1,10 +1,27 @@
 class CounterController < ApplicationController
   def index
     @counter = params[:counter].to_i || 0
+
+    respond_to do |format|
+      format.html # renders index.html.erb with application.html.erb layout
+      format.any(:swiftui_vml) do
+        response.content_type = "application/swiftui+vml"
+        render :index # will use application.swiftui_vml.erb layout
+      end
+    end
   end
 
   def increment
     @counter = params[:counter].to_i + 1
-    render turbo_stream: turbo_stream.replace("counter", partial: "counter", locals: { counter: @counter })
+
+    respond_to do |format|
+      format.html do
+        render turbo_stream: turbo_stream.replace("counter", partial: "counter", locals: { counter: @counter })
+      end
+      format.any(:swiftui_vml) do
+        response.content_type = "application/swiftui+vml"
+        render :increment # will use application.swiftui_vml.erb layout
+      end
+    end
   end
 end
